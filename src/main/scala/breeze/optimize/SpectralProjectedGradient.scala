2f24dac5e2d4413adfab93cc714106bf1ab69a1f
Martin Mauch <mauch@crealytics.de>
2013-10-27 13:55:21 +0100

    Switch to FirstOrderMinimizer worked, but test seem to be slower.

--- src/main/scala/breeze/optimize/SpectralProjectedGradient.scala
+++ src/main/scala/breeze/optimize/SpectralProjectedGradient.scala
@@ -35,60 +35,29 @@ class SpectralProjectedGradient[T, -DF <: StochasticDiffFunction[T]](
   type History = Double
   def correctedVector(x: T, g: T): T = projection(x + g) - x
   protected def initialHistory(f: DF, init: T): History = 1.0
-  protected def chooseDescentDirection(state: State, f: DF): T = ???
-  protected def takeStep(state: State, dir: T, stepSize: Double): T = ???
+  protected def chooseDescentDirection(state: State, f: DF): T = correctedVector(state.x, state.grad * -state.history)
+  protected def takeStep(state: State, dir: T, stepSize: Double): T = projection(state.x + dir * stepSize)
   protected def updateHistory(newX: T, newGrad: T, newVal: Double, f: DF, oldState: State): History = {
     val y = newGrad - oldState.grad
     val s = newX - oldState.x
     val alpha = s.dot(s) / s.dot(y)
-    if (alpha < alphaMin || alpha > alphaMax)
+    if (alpha.isNaN())
+      0.0
+    else if (alpha < alphaMin || alpha > alphaMax)
       1
     else
       alpha
   }
 
-  override def minimize(f: DF, init: T): T = {
-    val initX = if (initFeas) copy(init) else projection(copy(init))
-
-    var funEvals = 1
-    var currentState = initialState(f, initX)
-    breakable {
-      while (funEvals < maxIter) {
-        val oldState = currentState
-        val d = correctedVector(currentState.x, currentState.grad * -oldState.history)
-        val gtd = currentState.grad.dot(d)
-        if (gtd > -tolerance)
-          break;
-
-        val fmax = updateFValWindow(currentState, currentState.value)
-        currentState = State(currentState.x, currentState.value, currentState.grad, currentState.value, currentState.grad,oldState.iter + 1, currentState.value, updateHistory(currentState.x, currentState.grad, currentState.value, f, currentState), fmax, 0, false)
-        // Backtracking line-search
-        val res = determineStepSize(currentState, f, d)
-        val x = currentState.x + d * res
-        val value = f(x)
-        val grad = f.gradientAt(x)
-        funEvals += 5
-        var optCond = norm(correctedVector(x, -grad), 1)
-        if (optCond < tolerance)
-          break
-        if (norm(d, 1) < tolerance)
-          break
-        if (scala.math.abs(value - currentState.value) < tolerance)
-          break
-      }
-    }
-
-    currentState.x
-  }
   protected def determineStepSize(state: State, f: DF, direction: T): Double = {
     import state._
-    val funRef = fVals.max
+    val funRef = if (fVals.isEmpty) Double.PositiveInfinity else fVals.max
     var lineSearchIters = 0
     var t = if (iter == 0) {
-          scala.math.min(1.0, (1.0 / norm(grad, 1)))
-        } else {
-          1.0
-        }
+      scala.math.min(1.0, (1.0 / norm(grad, 1)))
+    } else {
+      1.0
+    }
     var xNew = x + direction * t
     var fNew = f(xNew)
     var gNew = f.gradientAt(xNew)
